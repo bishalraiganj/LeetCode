f5f389bd7360d549196ce01da77cc28e0986f538
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-04-05 17:09:19 +0530

    logic failing

--- BinaryRepresentationsSubString/src/Adhikary/X/Main.java
+++ BinaryRepresentationsSubString/src/Adhikary/X/Main.java
@@ -1,12 +1,20 @@
 package Adhikary.X;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
+    static Map<String,String> allBinaries=new LinkedHashMap<>();
     public static void main(String... args)
     {
+
+        System.out.println(convertToBinary(15));
+        System.out.println("-".repeat(50));
+
+        System.out.println(queryString("110101011011000011011111000000", 15));
+
+        System.out.println(allBinaries.toString());
+
 
 
 
@@ -15,41 +23,84 @@ public class Main {
     public static boolean queryString(String s, int n)
     {
         boolean status = false;
-        if(s.length()<=1000&&s.length()>=1&&n>=1&&n<=Math.pow(10,9)) {
+        boolean stringStatus = false;
+        for (Character c : s.toCharArray())
+        {
+            if(c=='0'||c=='1') {
+                stringStatus = true;
+                continue;
+            }
+            stringStatus = false;
+        }
+
+        if(stringStatus&&s.length()<=1000&&s.length()>=1&&n>=1&&n<=Math.pow(10,9)) {
             for (int i = 1; i <= n; i++) {
 
-                double x = Math.log(i)/Math.log(2);
-                double due=i;
-                int curr = (int) Math.floor(Math.log(due)/Math.log(2));
-                List<Integer> binaryArr = new ArrayList<Integer> ();
-                if(x>1&&i!=2||i!=1)
+                if(s.contains(convertToBinary(i)))
                 {
-                    while(curr>1)
-                    {
-                        due = i-(Math.pow(2,curr));
-                        binaryArr.add(curr,1);
-
-                    }
-                    if(due==1)
-                    {
-                        binaryArr.add(0,1);
-                    }
-                    if(due==2)
-                    {
-                        binaryArr.add(1,2);
-                    }
-
-
+                    allBinaries.put(i+"",convertToBinary(i));
+                    status = true;
+                    continue;
                 }
+                status =false;
+            }
+        }
+        return status;
+    }
+
+    public static String convertToBinary(int n)
+    {
+
+        if(n==0)
+        {
+            return "0";
+        }
+        if(n==1)
+        {
+            return "1";
+        }
+        if(n==2)
+        {
+            return "10";
+        }
+
+        double x = Math.log(n)/Math.log(2);
+        double due=n;
+        int curr = (int) Math.floor(Math.log(due)/Math.log(2));
+        List<Integer> binaryArr = new ArrayList<Integer>(Collections.nCopies(curr+1,0));
+        if(x>1)
+        {
+            while(curr>1)
+            {
+                due = due-(Math.pow(2,curr));
+                binaryArr.set(curr,1);
+                curr = (int) Math.floor(Math.log(due)/Math.log(2));
 
 
             }
+            if(due==1)
+            {
+                binaryArr.set(0,1);
+            }
+            if(due==2)
+            {
+                binaryArr.set(1,1);
+            }
+            if(due==3)
+            {
+                binaryArr.set(1,1);
+                binaryArr.set(0,1);
+            }
+//            binaryArr.reversed();
+
         }
 
-
-
-
-
+        return binaryArr.stream()
+                .map((i)->i+"")
+                .collect(()->new ArrayList<>(),(ArrayList<String> e1,String e2)->e1.add(e2),(e3,e4)->e3.addAll(e4)).reversed() // This reversed () method works , because i am using Java 21+ older versions do not return the reversed immutable view of the list
+//   Algorithm Written and Designed by Bishal Adhikary :-)
+                .stream()
+                .reduce("",(s,s1)->s+""+s1);
     }
 
 
