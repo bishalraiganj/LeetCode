5a4c766c8d05c0efa54820ff1aed348d92205244
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-06-03 02:25:11 +0530

    in middle of logic of compute()

--- Candy/src/Adhikary/X/Main.java
+++ Candy/src/Adhikary/X/Main.java
@@ -1,6 +1,11 @@
 package Adhikary.X;
 
+import com.sun.source.tree.Tree;
+
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
@@ -26,6 +31,63 @@ public class Main {
 			{
 				return -1;
 			}
+		}
+
+
+
+	}
+
+	public static int compute(int[] ratings)
+	{
+		int iterationCounter = 1;
+
+		int[] copy = new int[ratings.length];
+
+		Map<Integer, Integer> map = new TreeMap<>();
+		for(int i : ratings)
+		{
+			map.put(ratings[i],i);
+		}
+
+		for(Map.Entry<Integer,Integer> e : map.entrySet())
+		{
+			if(iterationCounter == 1)
+			{
+				copy[e.getValue()]=1;
+			}
+			else
+			{
+				if(e.getValue() == 0 ) {
+					if (e.getKey() > ratings[1]) {
+						copy[0] = ratings[1] + 1;
+						iterationCounter++;
+					} else if(e.getKey() <= ratings[1])
+					{
+						copy[0] = 1;
+					}
+				}
+				else if(e.getValue() == ratings.length - 1)
+				{
+					if(e.getKey() > ratings[e.getValue() - 1]) {
+						copy[e.getValue()] = ratings[e.getValue() - 1] + 1;
+					}
+					else
+					{
+						copy[e.getValue()] = 1 ;
+					}
+
+				}
+				else
+				{
+
+
+
+
+				}
+
+
+			}
+
 		}
 
 
