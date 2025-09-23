bd75e948b10d60e7a1172777a6c8fa3bbd067bcd
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-09-23 22:48:36 +0530

    Very Complex approach , unrealistic unusable

--- Interview150/Stack/Simplify_Path_71.java
+++ Interview150/Stack/Simplify_Path_71.java
@@ -1,56 +1,134 @@
 package Interview150.Stack;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class Simplify_Path_71 {
 
-	public String simplifyPath(String path) {
 
-		// absolute path "/home/user/Documents/../Pictures"
+	public static void main(String... args)
+	{
+		String s1 = "/home/user/Documents/../Pictures";
+		String s2 = "/../";
+		String s3 ="/.../a/../../b/c/../d/./";
+		String s4 = "/a//b////c/d//././/..";
+		String s5 = "/";
 
-		if("".equals(path))
-		{
-			return "";
-		}
-		Deque<Character> stack = new ArrayDeque<>();
 
-		char[] chars = path.toCharArray();
-		char prev ='\u0000';
-		String canonicalPath = "";
+		System.out.println(simplifyPath(s4));
 
-		int strLength = path.length();
 
-		int i = 0;
-		while( i < strLength)
-		{
-			char curr = path.charAt(i);
-			if(prev!=curr && curr =='/')
-			{
-				stack.push('/');
-			}
-			else if(   !(65<= (int) prev && (int) prev <=90) &&   !(97<= (int) prev && (int) prev <=122 )
-			&&  65<= (int) curr && (int) curr <=90 ||   97<= (int) curr && (int) curr <=122)
-			{
-				if(!stack.isEmpty()) {
-					char fwdSlash = stack.pop();
-				}
 
-				canonicalPath+="/"+curr;
-				while( ++i < strLength)
+	}
+
+	public static String simplifyPath(String path) {
+
+
+		if(path!= null && !path.equals("")) {
+
+			// absolute path "/home/user/Documents/../Pictures"
+			//   " /../"
+			Deque<Character> stack = new ArrayDeque<>();
+
+
+			StringJoiner currDirectory = new StringJoiner("");
+			char prevChar = '\u0000';
+			char currChar = '\u0000';
+
+			int length = path.length();
+			int i = 0;
+
+			String[] dirArr = new String[15];
+			int counter = 0;
+
+			while (i < length) {
+				currChar = path.charAt(i);
+
+
+				//"/home/user/Documents/../Pictures"
+				//   "/../"
+				if (prevChar != '/' && currChar == '/')
 				{
-					curr = path.charAt(i);
-					if(65<= (int) curr && (int) curr <=90 ||   97<= (int) curr && (int) curr <=122)
-					{
-						canonicalPath+= curr;
-					}
-					break;
-				}
 
+					String currDirString = currDirectory.toString();
+//				if(!currDirectory.toString().equals("/..") )
+//				if(  !currDirectory.toString().equals("/.."))
+//				{
+//					canonicalPath.add(dirArr[counter++]);
+//				}
+//				else
+
+					System.out.println(currDirString);
+					if (currDirString.equals("/..")) {
+//					System.out.println(Arrays.toString(dirArr));
+//					System.out.println(currDirString);
+//					System.out.println(counter + " " + dirArr[counter-1]);
+						if(counter!=0) {
+							dirArr[--counter] = "";
+						}
+					}
+//					if(currDirString.equals("/.") || currDirString.equals("/.."))
+//					{
+//						System.out.println(Arrays.toString(dirArr));
+//						System.out.println(currDirString);
+//						System.out.println(counter + " " + dirArr[counter-1]);
+//					}
+
+					if (!currDirString.equals("/..")&& !(currDirString.equals("/.")) && (!currDirString.isEmpty()) && (!currDirString.equals(" "))) {
+
+
+//					counter++;
+						dirArr[counter++] = currDirString;
+					}
+
+
+					stack.push('/');
+
+
+					currDirectory = new StringJoiner("");
+
+					prevChar = '/';
+
+				} else if (currChar != '/') {
+					if (prevChar == '/' && !stack.isEmpty()) {
+						char fwdSlash = stack.pop();
+						currDirectory.add("/");
+					}
+					currDirectory.add("" + currChar);
+					prevChar = currChar;
+				}
+				i++;
+
+				if (i == length) {
+					if(path.equals("/../"))
+					{
+						return "/";
+					}
+					if (currDirectory.toString().equals("/..")) {
+						dirArr[--counter] = "";
+					} else {
+						dirArr[counter] = currDirectory.toString();
+					}
+				}
+			}
+
+
+			StringJoiner canonicalPath = new StringJoiner("");
+
+			System.out.println(counter + ""+(dirArr[counter].isEmpty()));
+			for (int j = 0; j <= counter; j++) {
+				canonicalPath.add(dirArr[j]);
 
 			}
 
+
+		System.out.println(Arrays.toString(dirArr));
+
+
+			return canonicalPath.toString();
 		}
+
+		return "";
+
 
 
 	}
