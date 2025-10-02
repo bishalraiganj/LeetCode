3e69ffd6ff33cfa3224f2dd1b9675637acfa5e13
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-10-02 15:06:52 +0530

    112 Path sum : Recursive DFS + Backtracking  + pre-order

---
+++ Interview150/Tree/Binary_Tree/Path_Sum_112.java
@@ -0,0 +1,63 @@
+package Interview150.Tree.Binary_Tree;
+
+public class Path_Sum_112 {
+
+	int currTotal = 0;
+	int targetSum =0;
+	int subArrayDecreaseSum = 0;
+	public boolean hasPathSum(TreeNode root, int targetSum) {
+
+		//Edge Cases Handling
+		if(root==null || (root.left==null && root.right==null && root.val!=targetSum))
+		{
+			return false;
+		}
+
+		this.targetSum = targetSum;
+		return inOrder(root);
+	}
+
+	public boolean inOrder(TreeNode root)
+	{
+		if(root.left==null && root.right==null)
+		{
+			currTotal += root.val;
+			return currTotal==targetSum;
+		}
+
+		currTotal+=root.val;
+		boolean leftFlag = false, rightFlag = false;
+
+		if(root.left!= null)  leftFlag = inOrder(root.left);
+		if(leftFlag)
+		{
+			return true;
+		}
+		else {
+			currTotal-=root.left!=null?root.left.val:0;
+			if (root.right != null) rightFlag = inOrder(root.right);
+			if(rightFlag)
+			{
+				return true;
+			}
+			else
+			{
+				currTotal-=root.right!=null?root.right.val:0;
+				return false;
+			}
+		}
+
+	}
+
+
+	public boolean hasPathSum2(TreeNode root, int targetSum)
+	{
+		if(root==null) return false;
+
+		if(root.left==null && root.right==null) return root.val == targetSum;
+
+		return (hasPathSum2(root.left,targetSum-root.val) || hasPathSum2(root.right,targetSum-root.val));
+
+	}
+
+}
