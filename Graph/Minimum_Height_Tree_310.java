ede0b92a7c868e3003917472b4271bc6b128b829
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-10-14 01:43:19 +0530

    310 Minimum Height Tree (MHT) , Topological Leaf Trimming

--- Graph/Minimum_Height_Tree_310.java
+++ Graph/Minimum_Height_Tree_310.java
@@ -1,14 +1,10 @@
 package Graph;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
-
-class Minimum_Height_Tree_310 {
+public class Minimum_Height_Tree_310 {
 	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
 		Map<Integer,List<Integer>> adj = new HashMap<>();
 
@@ -80,6 +76,70 @@ class Minimum_Height_Tree_310 {
 		}
 		return currHeight;
 	}
+
+
+
+
+
+	// Most Optimized : Topological Leaf Trimming
+	public List<Integer> findMinimumHeightTrees(int n, int[][] edges)
+	{
+		Map<Integer,List<Integer>> adj = new HashMap<>();
+		int[] degree = new int[n];
+
+		for(int i = 0; i < n ; i++)
+		{
+			adj.put(i, new ArrayList<>());
+		}
+
+		for(int[] edge : edges)
+		{
+			adj.get(edge[0]).add(edge[1]);
+			adj.get(edge[1]).add(edge[0]);
+			degree[edge[0]]++;
+			degree[edge[1]]++;
+		}
+
+
+		Queue<Integer> queue = new ArrayDeque<>();
+
+		for(int i = 0 ; i < n ; i++)
+		{
+			if(degree[i] == 1)
+			{
+				queue.offer(i);
+			}
+		}
+
+		List<Integer> lastLayer = new ArrayList<>();
+		while(!queue.isEmpty())
+		{
+			int size = queue.size();
+
+			lastLayer = new ArrayList<>();
+			for(int i = 0 ; i < size ; i++)
+			{
+				int leaf = queue.poll();
+
+				lastLayer.add(leaf);
+
+
+				for(int j : adj.get(leaf))
+				{
+					degree[j]--;
+					if(degree[j]==1)
+					{
+						queue.offer(j);
+					}
+				}
+			}
+		}
+		return lastLayer;
+	}
 }
+
+
+
+
 
 
