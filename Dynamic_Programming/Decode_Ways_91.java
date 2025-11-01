af61d9948d38aa84dabe7d728c0ffab6f873e675
Bishal Adhikary <bishaladhikaryreal@gmail.com>
2025-11-01 20:44:32 +0530

    91 Decode Ways (Top Down recursive Memoization )

---
+++ Dynamic_Programming/Decode_Ways_91.java
@@ -0,0 +1,69 @@
+package Dynamic_Programming;
+
+public class Decode_Ways_91 {
+}
+
+
+
+class Solution {
+	public int numDecodings(String s) {
+
+		int len = s.length();
+		int[] dp = new int[len];
+		for(int i = 0  ;  i < len ; i++)
+		{
+			dp[i] = -1;
+		}
+
+
+		return memoize(s.length()-1,s,dp);
+	}
+
+
+
+
+	//Memoization(Recursive Top-Down DP)
+	public int memoize(int idx,String s, int[] dp)
+	{
+		if(idx >=0 &&  dp[idx]!=-1 ) return dp[idx];
+		if(idx<0) return 1;
+		if(s.charAt(0) == '0') return 0;
+		if(idx == 0  )
+		{
+			int num = s.charAt(0) - '0';
+			if(num >= 1 && num <= 9) {
+				dp[0] = 1;
+				return 1;
+			}
+		}
+
+
+		int netWays = 0;
+		int curr = s.charAt(idx)-'0';
+
+		if(curr >=1 && curr<= 9)
+		{
+			if(idx-1>=0)
+				netWays+= memoize(idx-1 ,s,dp);
+		}
+
+
+
+		if(idx-1 >=0 && s.charAt(idx-1) != '0')
+		{
+			int appendedPrev = ((s.charAt(idx-1)-'0') * 10) +  (s.charAt(idx)-'0');
+			if(appendedPrev >= 10 && appendedPrev <=26)
+			{
+				netWays+= memoize(idx-2,s,dp);
+			}
+		}
+
+
+		dp[idx] = netWays;
+		return netWays;
+	}
+
+
+
+
+}
